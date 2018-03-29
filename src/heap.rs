e91bf77e3e69d141ca262204537715a832da0544
Mike Hommey <mh@glandium.org>
2018-03-29 22:17:34 +0900

    Add #[inline] annotations to the various From impls in heap.rs

--- src/heap.rs
+++ src/heap.rs
@@ -88,18 +88,21 @@ unsafe impl our::Alloc for Heap {
 }
 
 impl From<our::Layout> for their::Layout {
+    #[inline]
     fn from(l: our::Layout) -> Self {
         unsafe { their::Layout::from_size_align_unchecked(l.size(), l.align()) }
     }
 }
 
 impl From<their::Layout> for our::Layout {
+    #[inline]
     fn from(l: their::Layout) -> Self {
         unsafe { our::Layout::from_size_align_unchecked(l.size(), l.align()) }
     }
 }
 
 impl From<their::AllocErr> for our::AllocErr {
+    #[inline]
     fn from(e: their::AllocErr) -> Self {
         match e {
             their::AllocErr::Exhausted { request: r } => our::AllocErr::Exhausted { request: r.into() },
@@ -109,6 +112,7 @@ impl From<their::AllocErr> for our::AllocErr {
 }
 
 impl From<our::AllocErr> for their::AllocErr {
+    #[inline]
     fn from(e: our::AllocErr) -> Self {
         match e {
             our::AllocErr::Exhausted { request: r } => their::AllocErr::Exhausted { request: r.into() },
@@ -118,12 +122,14 @@ impl From<our::AllocErr> for their::AllocErr {
 }
 
 impl From<their::Excess> for our::Excess {
+    #[inline]
     fn from(e: their::Excess) -> Self {
         our::Excess(e.0, e.1)
     }
 }
 
 impl From<their::CannotReallocInPlace> for our::CannotReallocInPlace {
+    #[inline]
     fn from(_: their::CannotReallocInPlace) -> Self {
         our::CannotReallocInPlace
     }
