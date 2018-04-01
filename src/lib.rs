63bc881bc5b767dd59c321b2ad0f287914717494
Mike Hommey <mh@glandium.org>
2018-04-02 08:54:35 +0900

    Rename the nightly_compat feature to unstable-rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 #![no_std]
-#![cfg_attr(feature = "nightly_compat", feature(alloc, allocator_api))]
+#![cfg_attr(feature = "unstable-rust", feature(alloc, allocator_api))]
 
-#[cfg(feature = "nightly_compat")]
+#[cfg(feature = "unstable-rust")]
 extern crate alloc;
-#[cfg(feature = "nightly_compat")]
+#[cfg(feature = "unstable-rust")]
 extern crate std;
 
 pub mod allocator;
 #[cfg(feature = "box")]
 pub mod boxed;
-#[cfg(feature = "nightly_compat")]
+#[cfg(feature = "unstable-rust")]
 pub mod compat;
 pub mod raw_vec;
 
