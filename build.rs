51026696df39cb0308f615cd6427e0f690b6bbf7
Mike Hommey <mh@glandium.org>
2018-05-08 16:06:43 +0900

    Change the version check in build.rs

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ extern crate rustc_version;
 use rustc_version::{version, Version};
 
 fn main() {
-    if version().unwrap() >= Version::parse("1.26.0-nightly").unwrap() {
+    if version().unwrap() >= Version::parse("1.26.0-alpha").unwrap() {
         println!("cargo:rustc-cfg=feature=\"i128\"");
         println!("cargo:rustc-cfg=feature=\"fused\"");
         println!("cargo:rustc-cfg=feature=\"unstable_name_collision\"");
